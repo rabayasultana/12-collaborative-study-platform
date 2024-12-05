ebc9484ee3bc26a3298f2872bfe969fc7d3492e6
RabayaSultana <rabayashova811@gmail.com>
2024-12-05 16:29:34 +0600

    dashboard section started

---
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -0,0 +1,14 @@
+import useAuth from "../../../hooks/useAuth";
+
+const AdminHome = () => {
+    const { user } = useAuth
+    return (
+        <div>
+            <h2>Admin home
+                {user?.displayName ? user.displayName : 'Back'}
+            </h2>
+        </div>
+    );
+};
+
+export default AdminHome;
---
+++ src/pages/Dashboard/StudentHome/StudentHome.jsx
@@ -0,0 +1,10 @@
+
+const StudentHome = () => {
+    return (
+        <div>
+            <h3>student home</h3>
+        </div>
+    );
+};
+
+export default StudentHome;
